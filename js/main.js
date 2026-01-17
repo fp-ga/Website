3a22b60dbf8e28369121628734619140310708d1
Mauro Brügger <mbruegger@student.ethz.ch>
2026-01-17 22:10:01 +0100

    changed colour and size

--- js/main.js
+++ js/main.js
@@ -12,15 +12,15 @@ function renderMaze(maze){
 	for(let i=0;i<maze.length;i++){
 		for(let j=0;j<maze[i].length;j++){
 			const cell = document.createElement("div");
-			cell.style.width = "20px";
-			cell.style.height = "20px";
+			cell.style.width = "50px";
+			cell.style.height = "50px";
 
 			switch(maze[i][j]){
 				case 0:
 					cell.style.backgroundColor = "#e0e1dd"; // walkable
 					break;
 				case 1:
-					cell.style.backgroundColor = "#1b263b"; // wall
+					cell.style.backgroundColor = "#0d1b2a";
 					break;
 				case 2:
 					cell.style.backgroundColor = "#4caf50";
