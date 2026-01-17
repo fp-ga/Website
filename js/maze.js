b351538b084380961622ff16e3ea69b39a7b7edc
Mauro Brügger <mbruegger@student.ethz.ch>
2026-01-17 21:21:48 +0100

    starting maze stuff

---
+++ js/maze.js
@@ -0,0 +1,36 @@
+//generates a maze Matrix. 1 signifies a wall 0 is air/walkable 
+//and 2 and 3 are start and end respectively
+//invariant: n>3
+function generateMaze(n){
+
+	const maze=[];
+	const mazeMirror=[];
+	for(let i=0;i<n;i++){
+		maze[i]=[];
+		for(let j=0;j<n;j++){
+			maze[i][j]=(Math.random()>0.7)?1:0;
+		}
+	}
+	//game of life stuff :D
+	for(let i=0;i<n;i++){
+                mazeMirror[i]=[];
+                for(let j=0;j<n;j++){
+                        if(i===0||j===0||i===n-1||j===n-1){
+				mazeMirror[i][j]=1;
+			}else{
+				const neighbourhood = maze[i-1][j-1]+
+					maze[i-1][j]+maze[i-1][j+1]+
+					maze[i][j-1]+maze[i][j+1]+
+					maze[i+1][j-1]+maze[i+1][j]+
+					maze[i+1][j+1];
+				if(neighbourhood<2||neighbourhood>6){
+					mazeMirror[i][j]=1;
+				}
+			}
+                }
+        }
+	mazeMirror[1][1]=2;
+	mazeMirror[n-2][n-2]=3;
+	return mazeMirror;
+}
+window.generateMaze=generateMaze;
